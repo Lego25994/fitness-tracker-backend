09e25c7b1681f68a5148ee639dd59cde8453a964
ocawesome101 <ocawesome101@gmail.com>
2021-01-18 16:51:09 -0500

    we now have rudimentary user ID'ing through postgresql

--- app.lua
+++ app.lua
@@ -1,4 +1,6 @@
 local lapis = require("lapis")
+local model = require("lapis.db.model").Model
+local users = model:extend("users")
 
 local app = lapis.Application()
 app:enable("etlua")
@@ -8,15 +10,25 @@ app:get("/", function()
 end)
 
 app:get("/home", function(self)
-  if not self.session.current_user then
+  if not self.session.current_user_id then
     return { redirect_to = "/" }
   else
+    self.current_user = users:find(self.session.current_user_id)
     return { render = "home" }
   end
 end)
 
 app:post("/login", function(self)
-  self.session.current_user = self.params.username
+  local user = self.params.username
+  local matches = users:select("where username = ?", user)
+  if #matches == 0 then
+    local new = users:create({
+      username = user
+    })
+    self.session.current_user_id = new.id
+  else
+    self.session.current_user_id = matches[1].id
+  end
   return { redirect_to = "/home" }
 end)
 

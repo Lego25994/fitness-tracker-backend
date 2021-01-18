9a2410057bcb7e1e2361e29126aac1eb4714ceeb
ocawesome101 <ocawesome101@gmail.com>
2021-01-18 16:15:29 -0500

    we have username detection now!

--- app.lua
+++ app.lua
@@ -4,7 +4,20 @@ local app = lapis.Application()
 app:enable("etlua")
 
 app:get("/", function()
-  return { render = "hello" }
+  return { render = "login" }
+end)
+
+app:get("/home", function(self)
+  if not self.session.current_user then
+    return { redirect_to = "/" }
+  else
+    return { render = "home" }
+  end
+end)
+
+app:post("/login", function(self)
+  self.session.current_user = self.params.username
+  return { redirect_to = "/home" }
 end)
 
 return app

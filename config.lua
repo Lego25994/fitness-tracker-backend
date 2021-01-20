4d71047f1dd75928ae1c31cae9379e81938dd0bb
Matt Burke <spraints@gmail.com>
2021-01-20 22:54:18 +0000

    A naive attempt to get connected to the db in docker

--- config.lua
+++ config.lua
@@ -1,12 +1,21 @@
 local config = require("lapis.config")
 
+function getenv(name, default) {
+  local val = os.getenv(name)
+  if not val then
+    return default
+  else
+    return val
+  end
+}
+
 config("development", {
   session_name = "hot_dog_fitness_session",
   secret = "VERY SECRET KEEP OUT DONT TOUCH",
   postgres = {
-    host = "127.0.0.1",
-    user = "postgres",
-    password = "",
-    database = "hot_dog_fitness"
+    host = getenv("DATABASE_HOST", "127.0.0.1"),
+    user = getenv("DATABASE_USER", "postgres"),
+    password = getenv("DATABASE_PASSWORD", ""),
+    database = getenv("DATABASE_DATABASE", "hot_dog_fitness")
   }
 })

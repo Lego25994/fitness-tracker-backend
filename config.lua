34b3dd9443717a0fd63f7319363312315fd42e83
Matt Burke <spraints@gmail.com>
2021-01-27 18:28:16 -0500

    Get database connection to work

--- config.lua
+++ config.lua
@@ -1,6 +1,6 @@
 local config = require("lapis.config")
 
-function getenv(name, default)
+function cfg_getenv(name, default)
   local val = os.getenv(name)
   if not val then
     return default
@@ -13,9 +13,9 @@ config("development", {
   session_name = "hot_dog_fitness_session",
   secret = "VERY SECRET KEEP OUT DONT TOUCH",
   postgres = {
-    host = getenv("DATABASE_HOST", "127.0.0.1"),
-    user = getenv("DATABASE_USER", "postgres"),
-    password = getenv("DATABASE_PASSWORD", ""),
-    database = getenv("DATABASE_DATABASE", "hot_dog_fitness")
+    host = cfg_getenv("DATABASE_HOST", "127.0.0.1"),
+    user = cfg_getenv("DATABASE_USER", "postgres"),
+    password = cfg_getenv("DATABASE_PASSWORD", ""),
+    database = cfg_getenv("DATABASE_DATABASE", "hot_dog_fitness")
   }
 })
